413a95af9d2386bde4ff158aefeb909b6080ee53
sonia <soniayjw@gmail.com>
2024-08-31 05:56:25 +0800

    add animation

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init({
+  useClassNames: true,
+  initClassName: false,
+  animatedClassName: "animate__animated",
+});
 
 $(document).ready(function () {
-  $('.tab-item').on('click', function (){
-    let target = $(this).data('target');
+  $(".tab-item").on("click", function () {
+    let target = $(this).data("target");
 
-    $('.tab-item').removeClass('active');
-    $('.tab-content').removeClass('active');
+    $(".tab-item").removeClass("active");
+    $(".tab-content").removeClass("active");
 
-    $(this).addClass('active');
-    $(target).addClass('active');
-  })
-
-
+    $(this).addClass("active");
+    $(target).addClass("active");
+  });
 });
